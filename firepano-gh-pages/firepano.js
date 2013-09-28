037677d0ac072e28babb79d62fcd629d39f1ba88
Jared Rosen <jaredarosen@gmail.com>
2013-09-28 15:04:28 -0400

    index

--- firepano-gh-pages/firepano.js
+++ firepano-gh-pages/firepano.js
@@ -1,5 +1,5 @@
 var spinner = new Spinner({color: '#ddd'});
-var firebaseRef = 'https://firepano.firebaseio.com/';
+var firebaseRef = 'https://kit-demo.firebaseio.com/';
 
 function handleFileSelect(evt) {
   var f = evt.target.files[0];
