19754ef3d6a080f3eb4f0a0a76c58ce3a0112ccb
Jared Rosen <jaredarosen@gmail.com>
2013-10-01 15:57:57 -0400

    edit page updates

--- edit.php
+++ edit.php
@@ -11,10 +11,17 @@
 	<body>
 		<script>
 			var sessionID = getCookie("sessionID");
+			var url = "https://kit-demo.firebaseio.com/person/" + sessionID;
+			var personRef = new Firebase(url);
+			personRef.on('value', function(snapshot) {
+				$('#name').html(snapshot.val().first + ' ' + snapshot.val().last);
+				$('#bio').val(snapshot.val().bio);
+				$('#pic').attr('src', snapshot.val().pic);
+			});
 		</script>
-		<!-- <img class="picture" id="headshot" /> -->
-		<h1>Jared Rosen</h1>
-		<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>
+		<h1 id="name"></h1>
+		<img id="pic" src="">
+		<input type="text" name="bio" id="bio">
 	</body>
 </html>
 
