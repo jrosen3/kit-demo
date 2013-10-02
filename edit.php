a30ac14705b152b013f691a33837299602e37d4f
Jared Rosen <jaredarosen@gmail.com>
2013-10-02 00:22:39 -0400

    file uploader = check

--- edit.php
+++ edit.php
@@ -10,17 +10,33 @@
 
 	<body>
 		<script>
-			var sessionID = getCookie("sessionID");
-			var url = "https://kit-demo.firebaseio.com/person/" + sessionID;
-			var personRef = new Firebase(url);
-			personRef.on('value', function(snapshot) {
-				$('#name').html(snapshot.val().first + ' ' + snapshot.val().last);
-				$('#bio').val(snapshot.val().bio);
-				$('#pic').attr('src', snapshot.val().pic);
+			$(document).ready(function(){
+				var sessionID = getCookie("sessionID");
+				var url = "https://kit-demo.firebaseio.com/person/" + sessionID;
+				var personRef = new Firebase(url);
+				personRef.on('value', function(snapshot) {
+					$('#name').html(snapshot.val().first + ' ' + snapshot.val().last);
+					$('#bio').val(snapshot.val().bio);
+					$('#pic').attr('src', snapshot.val().picture);
+				});
+
+				$('#file-upload').on('change', function(e){
+					var file = e.target.file[0];
+					var reader = new FileReader();
+					reader.onload = function(e) {
+						var filePayload = e.target.result;
+						personRef.update({picture: filePayload});
+					};
+					reader.readAsDataURL(file);
+					$('#pic').attr('src', snapshot.val().picture);
+				});
+
 			});
 		</script>
+
 		<h1 id="name"></h1>
 		<img id="pic" src="">
+		<input type="file" accept="image/*" id="file-upload">
 		<input type="text" name="bio" id="bio">
 	</body>
 </html>
